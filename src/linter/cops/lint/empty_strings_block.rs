6f999ff1925d4b6f483dcdcdfbaad116a9a3d055
ellath3may0 <bleakbob22@gmail.com>
2026-06-16 06:29:53 -0500

    docs(src/linter/cops/lint): added documentation to rules

--- src/linter/cops/lint/empty_strings_block.rs
+++ src/linter/cops/lint/empty_strings_block.rs
@@ -8,10 +8,20 @@ use crate::linter::{
 pub struct LintEmptyStringsBlock;
 
 impl Rule for LintEmptyStringsBlock {
+    /// Returns the name of the rule - "Lint/EmptyStringsBlock"
     fn name(&self) -> &'static str {
         "Lint/EmptyStringsBlock"
     }
 
+    /// Checks for compliance with the rule
+    /// 
+    /// This rule is violated if any `RuleNode`s have an empty strings block.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `context` (`&LintContext`) - A `LintContext` containing the
+    /// `RuleNode`s to be checked
+    /// * `findings` (`&mut Vec<Finding>`) - Vector to push Finding to
     fn check(&self, context: &LintContext, findings: &mut Vec<Finding>) {
         for rule in &context.file.rules {
             if rule.strings.is_empty() {
